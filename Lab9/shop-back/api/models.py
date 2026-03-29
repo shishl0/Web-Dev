34c304cc0efcf402fddb5e91211fd0203e085e0a
shishl0 <bizhan.kanatov@gmail.com>
2026-03-29 14:33:00 +0500

    Added Lab9

---
+++ Lab9/shop-back/api/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField()
+    count = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
+    def __str__(self):
+        return self.name
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count,
+            'is_active': self.is_active,
+            'category': self.category.name if self.category else None,
+            'image': 'https://placehold.co/300x200/png?text=Product',
+            'images': ['https://placehold.co/300x200/png?text=Product'],
+            'rating': 5,
+            'link': 'https://kaspi.kz/'
+        }
